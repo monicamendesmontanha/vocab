093263aedfbd40b691b28ee03316d62a65c99eb8
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-24 16:10:50 +1000

    Add db to server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,34 @@
-const express = require("express");
-const PORT = process.env.PORT || 3000;
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+global.Vocab = require('./api/models/vocabModel');
+const routes = require('./api/routes/vocabRoutes');
 
-// mongodb+srv://Monica:<password>@vocab-amf5b.mongodb.net/test?retryWrites=true
+// for old Node before Promise were part of JS
+mongoose.Promisse = global.Promise;
+mongoose.set('useFindAndModify', false);
+const dbURL = `mongodb+srv://Monica:${ process.env.DB_PW }@vocab-amf5b.mongodb.net/test?retryWrites=true`;
+mongoose.connect(
+  dbURL,
+  { useNewUrlParser: true }
+);
+
+const PORT = process.env.PORT || 3000;    // Future proof for deployment
 const server = express();
 
+// Middleware
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
 
-server.get("/", (req, res) => {
-  res.send("on...");
-});
-
+routes(server);
 //start listening
+server.listen(PORT);
 
-server.listen(PORT, function() {
-  console.log("now listening for requests");
+server.use((req, res) => {
+  res.status(404).send({ url: `${req.originalUrl} not found`});
 });
 
-
-//configure everything in express
+console.log(`Server started on http://localhost:${ PORT }/`)
