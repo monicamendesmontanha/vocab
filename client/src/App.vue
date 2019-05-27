e16b73ff8ea4f650f47eac9f87bfb365e55a51b0
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-27 11:04:59 +1000

    Add inital template, script and style to the app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,68 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+    <div class="ui inverted segment navbar">
+        <div class="ui center aligned container">
+          <div class="ui large secondary inverted pointing menu compact">
+            <router-link to="/words" exact class="items">
+              <i class="comment outline icon"></i> Words
+            </router-link>
+            <router-link to="/words/new" exact class="item">
+              <i class="plus circle icon"></i> New
+            </router-link>
+            <router-link to="/quiz" exact class="item">
+              <i class="graduation cap icon"></i> Quiz
+            </router-link>
+          </div>
+        </div>
     </div>
-    <router-view/>
+
+    <div class="ui text container">
+      <div class="ui one column grid">
+        <div class="cloumn">
+          <router-view />
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+#app > div.navbar {
+  margin-bottom: 1.5em;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.myFlash {
+  width: 250px;
+  margin: 10px;
+  position: absolute;
+  top: 50;
+  right: 0;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+input {
+  width: 300px;
 }
+
+div.label {
+  width: 120px;
+}
+
+div.input {
+  margin-bottom: 10px;
+}
+
+button.ui.button {
+
+}
+
 </style>
+
