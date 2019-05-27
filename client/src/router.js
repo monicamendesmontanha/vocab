70c402cac72e95ac32ca0497737c12a56d7e6a79
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-27 11:07:35 +1000

    Fix route

--- client/src/router.js
+++ client/src/router.js
@@ -5,7 +5,7 @@ import Words from './views/Words.vue';
 import New from './views/New.vue';
 import Show from './views/Show.vue';
 import Edit from './views/Edit.vue';
-import Test from './views/Test/vue';
+import Test from './views/Test.vue';
 
 Vue.use(Router)
 
